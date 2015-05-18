9f0c57f29bd152aae6ebfe2eeb9ba21ff01fe17a
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-05-18 18:04:51 +0200

    clean code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,98 +10,27 @@ import scala.io.Source
 
 object Application extends Controller {
 
-  val vpName = Map(
-    "vp1" -> "Souvenir n°1",
-    "vp2" -> "Bref.",
-    "vp3" -> "J'étais en train de mater une vidéo de Canal...",
-    "vp4" -> "Réplique culte n°1",
-    "vp5" -> "Réplique culte n°2",
-    "vp6" -> "Réplique culte n°3",
-    "vp7" -> "Souvenir n°2",
-    "vp8" -> "Tu m'écoutes?!",
-    "vp9" -> "Jean Jacques",
-    "vp10" -> "Oh non! n°1",
-    "vp11" -> "Oh non! n°2",
-    "vp12" -> "Oh non! n°3",
-    "vp13" -> "J'ai découvert ce qu'était un vrai commentateur...",
-    "vp14" -> "J'ai surpris mon père devant la télé...",
-    "vp15" -> "Bref Canal a 30 ans",
-    "vp16" -> "Souvenir n°3",
-    "vp17" -> ":D",
-    "vp18" -> "Algorithme super puissant"
-  )
-
   def index = Action {
     Ok(views.html.index())
   }
 
+  val vps = List("vp0")
 
 
   def getPlaylist() = Action { request =>
-    val parameters = request.queryString
-
-    val sortedParameters = parameters.toList.sortBy(t => t._1.substring(2).toInt)
-    Logger.info("Request(" + sortedParameters.map(p => p._2(0)).mkString(",") + ")")
-
-
-
     val length = "15"
-    val videoPath = "assets/videos/vp0/output.ts"
+    val videoPath = "assets/videos/vp0/bonus_canal_cut_test.ts"
 
-    val playList = List(
-      "#EXTM3U",
-      "#EXTINF:" + length,
-      videoPath).mkString("\n")
+    val videos = (for(vp <- vps.take(1)) yield {
+      List(
+        "\"#EXT-X-DISCONTINUITY",
+        "#EXTINF:" + length,
+        videoPath
+      )
+    }).flatten
+    val playList = List("#EXTM3U") ::: videos ::: List("#EXT-X-ENDLIST")
 
-
-
-    Ok(playList)
-  }
-
-  def getPlaylistHTML5() = Action { request =>
-    val parameters = request.queryString
-
-    val sortedParameters = parameters.toList.sortBy(t => t._1.substring(2).toInt)
-    Logger.info("Request(" + sortedParameters.map(p => p._2(0)).mkString(",") + ")")
-
-    val playList = for ((vp, option) <- sortedParameters) yield {
-      "http://bref30cdn.wildmoka.com/vidv2/" + option(0) + "_med.webm"
-    }
-
-
-    Ok(Json.toJson(playList))
-  }
-
-
-  def getVPs() = Action {
-    val dir = new File("resources/options-by-vp")
-    val vpFiles = dir.listFiles().sortBy(_.getName.substring(2).toInt)
-    val vps = for (vpFile <- vpFiles) yield {
-      val options = for (optionFile <- vpFile.listFiles()) yield {
-        var name = optionFile.getName
-        name = name.substring(0, name.length - 4)
-        val poster = "resources/poster-frames/" + name + "_med0.png"
-        (name, poster)
-      }
-      (vpName(vpFile.getName), options.toList)
-    }
-
-    val json = Json.toJson(
-      vps.map(vp => Json.toJson(Map(
-        "name" -> Json.toJson(vp._1),
-        "options" -> Json.toJson(
-          vp._2.map(option =>
-            Json.toJson(Map(
-              "id" -> Json.toJson(Json.toJson(option._1)),
-              "poster" -> Json.toJson(Json.toJson(option._2))
-            ))
-          )),
-        "selected" -> Json.toJson(vp._2(0)._1),
-        "visible" -> Json.toJson(true)
-      )))
-    )
-
-    Ok(json)
+    Ok(playList.mkString("\n"))
   }
 
   def getResource(file : String) = Action {
--- public/javascripts/configurator.js
+++ public/javascripts/configurator.js
@@ -8,48 +8,20 @@ brefApp.controller('ConfiguratorCtrl', function ($scope, $http) {
 
     $scope.activated = false;
 
-    $scope.vps = [{
-        options : [
-            {
-                poster : ""
-            }
-        ]
-    }];
-    $http.get("getvps").success(function(data) {
-        $scope.vps = data;
-    });
-
     $scope.hasFlash = false;
     var plugins = navigator.plugins;
     for (var i = 0; i < plugins.length; i++) {
         if (plugins[i].description.toLowerCase().indexOf("flash") > -1) {
-            $scope.hasFlash = true
+            $scope.hasFlash = true;
         }
     }
-
-
-
-    //$scope.hasFlash = false; // FIXME
-    $scope.hasFlash = true // FIXME
 
     $scope.generate = function() {
         $scope.activated = true;
 
-        var parameters = "";
-        for (var i = 0; i < $scope.vps.length; i++) {
-            var selectedOption = $scope.vps[i].selected;
-            if (i > 0) {
-                parameters += "&"
-            }
-            var vpid = i + 1;
-            parameters += "vp" + vpid + "=" + selectedOption.trim()
-        }
 
-        var playlistURL = "get-playlist?" + parameters;
+        var playlistURL = "get-playlist";
 
-        console.log(playlistURL);
-
-        console.log("flash? = " + $scope.hasFlash);
 
         if ($scope.hasFlash) { // Flash
 
@@ -90,51 +62,7 @@ brefApp.controller('ConfiguratorCtrl', function ($scope, $http) {
             });
 
         } else { // HTML5
-            console.log("HTML5 player");
-            $http.get("get-playlist-html5?" + parameters).success(function(data) {
-
-                var videos = data;
-
-                var videoPlayer = document.getElementById('videoPlayer');
-
-                window.MediaSource = window.MediaSource || window.WebKitMediaSource;
-
-                var mediaSource = new MediaSource();
-
-                var video = document.getElementById('videoPlayer');
-                video.src = window.URL.createObjectURL(mediaSource);
-
-                mediaSource.addEventListener('sourceopen', function(e) {
-                    var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
-                    console.log(sourceBuffer);
-                    sourceBuffer.appendBuffer(new Uint8Array(videos[0]));
-                    console.log(sourceBuffer);
-                    video.play();
-                    console.log(sourceBuffer);
-                    sourceBuffer.addEventListener('updateend', function(e) {
-                        mediaSource.endOfStream();
-                    });
-                    //for (var i = 0; i < videos.length; i++) {
-                    //    console.log(videos[i]);
-                    //    //if (i == videos.length - 1 ) {
-                    //    //    mediasource.endOfStream();
-                    //    //}
-                    //    //sourceBuffer.append(videos[i])
-                    //    sourceBuffer.appendBuffer(new Uint8Array(videos[i]))
-                    //}
-                    //start playing after video header recieved(i.e; first chunk)
-                    //call media.endOfStream(); before appending last chunk
-                }, false);
-
-
-
-//            var i = 1;
-//            videoPlayer.onended = function(){
-//                videoPlayer.src = BrefVideo[i];
-//                i++;
-//            }
-//                videoPlayer.src = BrefVideo[0];
-            });
+            console.log("Error: Flash not supported");
         }
     };
 });
