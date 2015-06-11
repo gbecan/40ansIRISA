edfac34aa98d00bd2cce486e181e1791f54b8fb7
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-06-11 17:00:57 +0200

    konami

--- public/javascripts/configurator.js
+++ public/javascripts/configurator.js
@@ -21,48 +21,58 @@ irisaApp.controller('ConfiguratorCtrl', function ($scope, $http) {
             $scope.counter = data.counter;
             var playlistURL = data.playlistURL;
 
+            $scope.play(playlistURL)
 
-            if ($scope.hasFlash) { // Flash
-
-                flowplayer("player", "assets/flash/flowplayer.swf", {
-                    wmode: 'direct',
-                    plugins: {
-                        httpstreaming: {
-                            url: 'assets/flash/flashlsFlowPlayer.swf',
-                            hls_debug : false,
-                            hls_debug2 : false,
-                            hls_lowbufferlength : 3,
-                            hls_minbufferlength : 5,
-                            hls_maxbufferlength : 0,
-                            hls_startfromlevel : -1,
-                            hls_seekfromlevel : -1,
-                            hls_live_flushurlcache : false,
-                            hls_seekmode : "ACCURATE",
-                            hls_fragmentloadmaxretry : -1,
-                            hls_manifestloadmaxretry : -1,
-                            hls_capleveltostage : false,
-                            hls_maxlevelcappingmode : "downscale"
-                        }
-                    },
-                    clip: {
-                        accelerated: true,
-                        url: playlistURL,
-                        urlResolvers: "httpstreaming",
-                        lang: "fr",
-                        provider: "httpstreaming",
-                        autoPlay: false,
-                        autoBuffering: true
-                    },
-                    log: {
-                        level: 'none',
-                        filter: 'org.flowplayer.controller.*'
-                    }
-                });
-
-            } else { // HTML5
-                console.log("Error: Flash not supported");
-            }
         });
 
     };
+
+    $scope.play = function (playlistURL) {
+        if ($scope.hasFlash) { // Flash
+
+            flowplayer("player", "assets/flash/flowplayer.swf", {
+                wmode: 'direct',
+                plugins: {
+                    httpstreaming: {
+                        url: 'assets/flash/flashlsFlowPlayer.swf',
+                        hls_debug : false,
+                        hls_debug2 : false,
+                        hls_lowbufferlength : 3,
+                        hls_minbufferlength : 5,
+                        hls_maxbufferlength : 0,
+                        hls_startfromlevel : -1,
+                        hls_seekfromlevel : -1,
+                        hls_live_flushurlcache : false,
+                        hls_seekmode : "ACCURATE",
+                        hls_fragmentloadmaxretry : -1,
+                        hls_manifestloadmaxretry : -1,
+                        hls_capleveltostage : false,
+                        hls_maxlevelcappingmode : "downscale"
+                    }
+                },
+                clip: {
+                    accelerated: true,
+                    url: playlistURL,
+                    urlResolvers: "httpstreaming",
+                    lang: "fr",
+                    provider: "httpstreaming",
+                    autoPlay: false,
+                    autoBuffering: true
+                },
+                log: {
+                    level: 'none',
+                    filter: 'org.flowplayer.controller.*'
+                }
+            });
+
+        } else { // HTML5
+            console.log("Error: Flash not supported");
+        }
+    };
+
+    new Konami(function() {
+        $scope.activated = true;
+        $scope.$apply();
+        $scope.play("get-playlist?vp0=0");
+    });
 });
